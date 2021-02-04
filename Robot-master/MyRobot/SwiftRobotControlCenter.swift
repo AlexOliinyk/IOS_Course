14e1cfd52b4507fabdbee659bd3751dbf2b9b84d
Oleksandr Oliinyk <alex.oliinyk@gmail.com>
2021-02-04 15:06:52 +0200

    clone robot

---
+++ Robot-master/MyRobot/SwiftRobotControlCenter.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftRobotControlCenter.swift
+//  MyRobot
+//
+//  Created by Ivan Vasilevich on 10/4/14.
+//  Copyright (c) 2014 Ivan Besarab. All rights reserved.
+//
+
+import UIKit
+//  All robot commands can be founded in GameViewController.h
+class SwiftRobotControlCenter: RobotControlCenter {
+    
+    //  Level name setup
+    override func viewDidLoad() {
+        levelName = "L0C" //  Level name
+        super.viewDidLoad()
+    }
+	
+	override func run() {
+		
+	}
+    
+}
